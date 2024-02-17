33f8c97a6f4f8ef7b1e48073cb5588113cc9a32c
fghwzw <2924074463@qq.com>
2024-02-17 16:05:21 +0800

    引入vant

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+// eslint-disable-next-line no-undef
+export default {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 375
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 在scss之前引入，后面覆盖
+import 'vant/lib/index.css'
+import './styles/main.scss'
 
 const app = createApp(App)
 
